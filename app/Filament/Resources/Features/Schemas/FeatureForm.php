202864c6d1a174a7cebe69047c518b9812291d95
EslamKamel89 <elsamkamelforex@gmail.com>
2026-06-29 01:28:52 +0300

    feat(features): auto-calculate cost based on effort and pricing toggle

--- app/Filament/Resources/Features/Schemas/FeatureForm.php
+++ app/Filament/Resources/Features/Schemas/FeatureForm.php
@@ -10,8 +10,10 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Slider;
 use Filament\Forms\Components\Slider\Enums\PipsMode;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use Illuminate\Validation\Rule;
 
@@ -53,6 +55,24 @@ class FeatureForm
                 RichEditor::make('description'),
                 TextInput::make('effort_in_days')
                     ->required()
+                    // ->live(debounce: 500)
+                    // ->afterStateUpdated(function ($state, Get $get, Set $set) {
+                    //     $effort = (int) $state;
+                    //     $isHightCost = $get('is_high_cost');
+                    //     if ($isHightCost) {
+                    //         $set('cost', $effort * 1500);
+                    //     } else {
+                    //         $set('cost', $effort * 1000);
+                    //     }
+                    // })
+                    ->afterStateUpdatedJs(<<<'JS'
+                        const isHighCost = $get('is_high_cost');
+                        if(isHighCost){
+                            $set('cost' , $state * 1500);
+                        } else {
+                            $set('cost' , $state * 1000);
+                        }
+                    JS)
                     ->numeric()
                     ->default(0),
                 Slider::make('priority')
@@ -70,6 +90,27 @@ class FeatureForm
                     ->numeric()
                     ->default(0)
                     ->prefix('$'),
+                Toggle::make('is_high_cost')
+                    // ->live()
+                    // ->afterStateUpdated(function ($state, Get $get, Set $set) {
+                    //     $effort = (int) $get('effort_in_days');
+                    //     if ($state) {
+                    //         $set('cost', $effort * 1500);
+                    //     } else {
+                    //         $set('cost', $effort * 1000);
+                    //     }
+                    // })
+                    ->dehydrated(false)
+                    ->afterStateUpdatedJs(<<<'JS'
+                    const isHighCost = $state ;
+                    const effort = $get('effort_in_days');
+                    if(isHighCost){
+                        $set('cost' , effort * 1500);
+                    }else {
+                        $set('cost' , effort * 1000);
+                    }
+                    JS)
+                    ->default(false),
                 DatePicker::make('delivered_at'),
             ]);
     }
